14993df0ed306df0038718a608cbae4880d0b10f
John <ojeahjohn2018@gmail.com>
2022-10-09 20:19:41 -0700

    my portfolio

---
+++ index.js
@@ -0,0 +1,10 @@
+let navd= document.getElementById("nav")
+let closse=document.getElementById("closr")
+function opend(){
+    navd.style.width="230px"
+    
+}
+closse.addEventListener("click", function(){
+    navd.style.width="0px"
+})
+
